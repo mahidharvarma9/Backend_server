beccd7b9d7e190ff4c33fd0fb2b483d12fb6fbf8
mahidharvarma9 <mahidharvarma9@gmail.com>
2020-08-01 22:39:29 +0530

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -45,6 +49,9 @@ app.use(express.urlencoded({ extended: false }));
 
 //app.use(cookieParser('12345-67890-09876-54321'));
 
+
+
+
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -53,27 +60,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
+    if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
-      return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
+      next(err);
     }
     else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+          next();
     }
-  }
 }
 
 app.use(auth);
